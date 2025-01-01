e2b37b3cf8ae85acb1ea4460f981460a508cb1b6
Your Name <you@example.com>
2025-01-01 23:00:25 +0800

    add h

--- src/components/CommingSoon/index.tsx
+++ src/components/CommingSoon/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ComingSoon: React.FC = () => {
   return (
-    <div className="flex items-center justify-center min-h-[75%] p-3">
+    <div className="flex items-center justify-center h-[70vh] p-3">
       <div className="text-center">
         {/* Big Gradient Text */}
         <h1 style={{ lineHeight: 1.5 }}className="text-6xl md:text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#17a2b7] via-[#7371b2] to-[#e847ec]">
